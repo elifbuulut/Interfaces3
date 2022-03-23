35159a13415415f6a5af60983101934589b89ca9
Elif Bulut <96698984+elifbuulut@users.noreply.github.com>
2022-03-23 04:05:42 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,24 +10,7 @@ namespace Interfaces3
     {
         static void Main(string[] args)
         {
-            // Bir class birden fazla Interfacei implimente edebilir. 
-            //--------------Çoklu implimentasyon Mantığı----------------------
-            // Şöyle düşünelim . Bir şirket var. Şirkette 3 tip çalışan var.Robot, Worker(çalışan), Manager(Yönetici).IWorker diye interface
-            //oluşturduğumuzu düşünelim. Bİr çalışan maaş alır, yemek yer,çalışır. Robot, Worker, Manager hepsi bir class olsun ve Iworker'ı 
-            //implimente etsin. (eğer bu bir inherit(kalıtım,miras) olsaydı inherit etsin diyecektik.). Evet, bir worker, bir manager eat ,get salary, 
-            //work gibi operasyonlar için uygundur ama bir robot uygun değildir. yemek yemez maaş almaz.AMA INTERFACE DE oluşturdugumuz şblon uymak
-            //ilk kurallarımızdan dolayısıyla robot classı için Iworkerı implimente ettiğimizde o şablon oldugu gibi gelecek (GetSalary,Eat,Work)
-            //Silemeyiz. O halde çözüm ÇOKLU İMPLEMENTASYONDUR. 
-            // yeme içme çalışma operasyonlarından interface oluşturur,uz 
-            /*
-                interface IWorker
-        {
-            void GetSalary();
-            void Eat();
-            void Work();
 
-        }
-            */
 
             Robot robot = new Robot();
             robot.Work();
